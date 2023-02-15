c25f687b92b735a57d11f99cd86d8d2aa5a976fb
JovanNaumov1 <mm_jovce@hotmail.com>
2023-02-15 21:06:46 +0100

    commit c2

---
+++ c2/index.js
@@ -0,0 +1,31 @@
+// console.log('zdravo svetu');
+// console.log(5 !=5);
+// console.log(typeof 25);
+
+// const chovek = {
+//     name: "Johnny",
+//     surename: "Bones",
+// };
+
+// console.log(chovek);
+
+// let niza = [1, true, 2.5, 'zravo'];
+// console.log(niza);
+
+let student = {
+    ime: "Pero",
+    prezime: "Nakov",
+    age: 25
+};
+console.log(student);
+
+student.prezime = "Perovski";
+
+console.log(student);
+
+function testFn() {
+    return 'Test fn initiated';
+}
+
+console.log(testFn());
+
